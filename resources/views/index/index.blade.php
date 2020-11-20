c1bc294a6df3a66a69d629f1c1bead0f6c0f4c02
myson <3304518720@qq.com>
2020-11-20 16:29:07 +0800

    xiaolv

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -200,7 +200,6 @@
         <div class="title">
             <h3 class="fl">猜你喜欢</h3>
             <b class="border"></b>
-            <a href="javascript:;" class="fr tip changeBnt" id="xxlChg"><i></i>换一换</a>
         </div>
         <div class="bd">
             <ul class="clearfix yui3-g Favourate picLB" id="picLBxxl">
@@ -212,13 +211,6 @@
                             <div class="like-text">
                                 <p>{{$v["goods_name"]}}</p>
                                 <h3>¥{{$v["goods_price"]}}</h3>
-                            </div>
-                        </dd>
-                        <dd>
-                            <a href="" class="pic"><img src="/status/img/like_01.png" alt="" /></a>
-                            <div class="like-text">
-                                <p>爱仕达 30CM炒锅不粘锅NWG8330E电磁炉炒</p>
-                                <h3>¥116.00</h3>
                             </div>
                         </dd>
                     </dl>
